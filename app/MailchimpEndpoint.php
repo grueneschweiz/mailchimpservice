93ffeb6e3143ddcc0638c3ae38603344076a864b
cyrillbolliger <cyrill.bolliger@gmail.com>
2019-05-07 14:30:32 +0200

    add mailchimp webhook endpoint

--- app/MailchimpEndpoint.php
+++ app/MailchimpEndpoint.php
@@ -13,7 +13,9 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $config
  * @property \DateTime $created_at
  * @property \DateTime $updated_at
+ *
  * @method static find( int $id )
+ * @method static where( string $string, string $secret )
  */
 class MailchimpEndpoint extends Model
 {
