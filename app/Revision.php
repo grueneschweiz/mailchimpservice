9b1415374452ebae1877226fb4fd0c4726e62b81
Cyrill Bolliger <mail@cyrill.me>
2022-03-30 16:51:14 +0200

    [FEAT] force sync all if no successful revision since X days

--- app/Revision.php
+++ app/Revision.php
@@ -4,12 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
  * Class Revision
  *
  *
  * @mixin Builder
+ * @property int $id
+ * @property int $revision_id
+ * @property string $config_name
+ * @property bool $sync_successful
+ * @property Carbon $updated_at
+ * @property Carbon $created_at
  * @package App
  */
 class Revision extends Model
