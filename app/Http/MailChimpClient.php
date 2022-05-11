88e7906b452e0c2f6818af11dd67609938e91169
Cyrill Bolliger <mail@cyrill.me>
2022-05-11 09:08:09 +0200

    [FEAT] improved logging

--- app/Http/MailChimpClient.php
+++ app/Http/MailChimpClient.php
@@ -257,7 +257,7 @@ class MailChimpClient
                 (isset($put['errors']) && strpos($put['errors'][0]['message'], 'is already in this list with a status of "Subscribed".'))
             ) {
                 $errors = isset($put['errors']) && !empty($put['errors'][0]['message']) ? " Errors: {$put['errors'][0]['message']}" : '';
-                throw new AlreadyInListException("{$put['detail']}$errors Email used for id calc: $email. Called endpoint: $endpoint. Data: " . print_r($mcData, true));
+                throw new AlreadyInListException("{$put['detail']}$errors Email used for id calc: $email. Called endpoint: $endpoint. Data: " . str_replace("\n", ', ', print_r($mcData, true)));
             }
             if (isset($put['detail']) && strpos($put['detail'], 'looks fake or invalid, please enter a real email address.')) {
                 throw new FakeEmailException($put['detail']);
